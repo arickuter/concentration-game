51f956c5ba52cad7ed3e5e84efc884d8ffd6e1fc
Aric <arickuter99@gmail.com>
2018-02-23 23:50:43 +0200

    Fixed entire game functionality

--- js/app.js
+++ js/app.js
@@ -4,10 +4,12 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
- $.getScript("lib.js");
 
 var cards = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-leaf", "fa fa-bicycle", "fa fa-bomb", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-leaf", "fa fa-bicycle", "fa fa-bomb"];
 var openCards = [];
+var moveNum = 0;
+var matchedPairs = 0;
+var clickable = true;
 
 $(document).ready(function() {
   shuffle(cards);
@@ -28,38 +30,86 @@ $(document).ready(function() {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 $('ul').on('click', 'li', function() {
-  showCard();
-
+  if (clickable === true) {
+    var activeCard = $(event.target);
+    showCard(activeCard);
+    checkCards(activeCard);
+  }
 });
 
-function showCard() {
-  let card = $(event.target);
-  $(card).css("pointer-events", "none");
-  card.addClass('show open');
-  checkCard(card);
+$('.restart').on('click', 'i', function() {
+  location.reload(true);
+});
+
+function showCard(activeCard) {
+  $(activeCard).css("pointer-events", "none");
+  activeCard.addClass('open show');
 }
 
-function checkCard(card) {
+function checkCards(activeCard) {
   if (openCards.length > 0) {
-    /* If card already selected */
-    var oldCard = $("." + openCards[0].substring(0, 2) + "." + openCards[0].substring(3, openCards[0].length));
-    if (card.children().attr('class') === openCards[0]) {
-      card.addClass('match');
-      oldCard.parent('li').addClass('match');
-      openCards = [];
-    } else {
-      openCards = [];
-      // Hot sleep for 2 seconds
-      start = new Date().getTime();
-      while (new Date().getTime() < start + 2000) {}
+    clickable = false;
+    if (activeCard.children().attr('class') === openCards[0].children().attr('class')) {
+      matchCards(activeCard, openCards[0]);
+      clickable = true;
 
-      oldCard.parent('li').removeClass('show open');
-      card.removeClass('show open');
-      $(oldCard.parent('li')).css("pointer-events", "auto");
-      $(card).css("pointer-events", "auto");
+    } else {
+      setTimeout(function() {
+        clickable = true;
+        noMatch(activeCard, openCards[0]);
+      }, 1250);
     }
-    /* If no card selected */
   } else {
-    openCards.push(card.children().attr('class'));
+    addCard(activeCard);
   }
+}
+
+function addCard(card) {
+  openCards.push(card);
+}
+
+function matchCards(cardOne, cardTwo) {
+  cardOne.addClass('match');
+  cardTwo.addClass('match');
+  matchedPairs += 1;
+  openCards = [];
+  setTimeout(function() {
+    win();
+  }, 1250);
+}
+
+function noMatch(cardOne, cardTwo) {
+  cardOne.removeClass('show open');
+  cardTwo.removeClass('show open');
+  $(cardOne).css("pointer-events", "auto");
+  $(cardTwo).css("pointer-events", "auto");
+  openCards = [];
+  move();
+}
+
+function move() {
+  moveNum += 1;
+  void(document.getElementById("moves").innerHTML = moveNum);
+}
+
+function win() {
+  if (matchedPairs === 8) {
+    alert('Congratulatons! You have won in ' + moveNum + ' moves!');
+  }
+}
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
